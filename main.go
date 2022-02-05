eeca4eda9d9c6276d7fc0e92188783aa20f46054
PetrPetr <ferel2015@yandex.ru>
2022-02-05 17:12:30 +0300

    I tweaked the code it got fucked up

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("ИНженерный калькулятор")
-	fmt.Println("Какое действие Вы хотите выполнить ?(+, -, *, /, %)")
+	fmt.Println("Engineering Calculator")
+	fmt.Println("What action do you want to perform ?(+, -, *, /, %)")
 
 	var action string
 	fmt.Scan(&action)
 	var a float64
 	var b float64
-	fmt.Println("Введите первое число:")
+	fmt.Println("Enter the first number:")
 	fmt.Scan(&a)
-	fmt.Println("Введите второе число:")
+	fmt.Println("Enter the Second number:")
 	fmt.Scan(&b)
 	switch {
 	case action == "+":
-		fmt.Println("Сумма чисел " + fmt.Sprint(a+b))
+		fmt.Println("Sum of numbers " + fmt.Sprint(a+b))
 
 	case action == "-":
-		fmt.Println("Сумма чисел " + fmt.Sprint(a-b))
+		fmt.Println("Sum of numbers " + fmt.Sprint(a-b))
 	case action == "*":
-		fmt.Println("Сумма чисел " + fmt.Sprint(a*b))
+		fmt.Println("Sum of numbers " + fmt.Sprint(a*b))
 	case action == "/":
 		fmt.Println("Сумма чисел " + fmt.Sprint(a/b))
 	case action == "%":
-		fmt.Println("Остаток от деления:" + fmt.Sprint(int64(a)%int64(b)))
+		fmt.Println("The remainder of the division:" + fmt.Sprint(int64(a)%int64(b)))
 	default:
 		fmt.Println("There are no  such numbers")
 	}
